282cea89963a19fd55f60ea731dc7eb130a97356
Nicholas <nhershy@users.noreply.github.com>
2026-02-02 15:48:11 -0600

    Initial commit

---
+++ AppToDmg/AppToDmgApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppToDmgApp.swift
+//  AppToDmg
+//
+//  Created by Nicholas Hershy on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct AppToDmgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AppToDmg
+Convert any macOS .app into a shareable .dmg installer in one click.
